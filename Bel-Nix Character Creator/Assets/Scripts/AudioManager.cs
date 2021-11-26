4f9b4c9b2f9bf2acf12706533d5cde0d9def56fe
Andrew <torre157@umn.edu>
2021-11-26 02:31:04 -0600

    Main Menu UI, audio transitions, video resolution options, and UI sprites added.

---
+++ Bel-Nix Character Creator/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public string audioSourceTag = "AudioSource";
+
+    AudioSource audioSource;
+    
+    //finds all audiosources in the game. determines a primary audio source at the first scene of game.  subsequent scenes 
+    //will have all other sources except the primary audiosource removed.
+    void FindAudioSources() {
+
+        AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+
+        if (audioSources.Length == 1)
+            TagPrimaryAudioSource(audioSources[0].gameObject);
+      
+
+        else 
+            RemoveOtherAudioSources(audioSources);
+        
+    }
+
+    //tags audio source as primary;
+    void TagPrimaryAudioSource(GameObject audioObject) {
+
+        audioObject.tag = audioSourceTag;
+        audioSource = audioObject.GetComponent<AudioSource>();
+        DontDestroyOnLoad(audioObject);
+
+    }
+
+    //removes all other audio sources that do not have the tag.
+    void RemoveOtherAudioSources(AudioSource[] audioSources) {
+
+        foreach (AudioSource a in audioSources)
+        {
+
+            if (a.gameObject.tag == audioSourceTag)
+                audioSource = a;
+
+            else
+                Destroy(a.gameObject);
+
+
+        }
+        
+    }
+    
+    private void Awake()
+    {
+
+        FindAudioSources();
+
+    }
+
+}
--- Bel-Nix Character Creator/Assets/Scripts/SceneTransitionManager.cs
+++ Bel-Nix Character Creator/Assets/Scripts/SceneTransitionManager.cs
@@ -6,18 +6,53 @@ using UnityEngine.SceneManagement;
 public class SceneTransitionManager : MonoBehaviour
 {
 
+    public Animator animator;
+    public Canvas fadeCanvas;
+
+    int nextSceneToLoad = 0;
+
     public void QuitGame() {
 
         Application.Quit();
 
     }
 
-    public void LoadScene(int scene) {
+    public void TransitionToNextScene(int sceneID) {
 
-        SceneManager.LoadScene(scene);
+        SetNextSeceneToLoad(sceneID);
+        SceneFadeOut();
         
     }
 
+    public void LoadScene() {
+
+        SceneManager.LoadScene(nextSceneToLoad);
+        
+    }
+
+    void SetNextSeceneToLoad(int sceneID) {
+
+        nextSceneToLoad = sceneID;
+        
+    }
+
+
+    public void SceneFadeIn(Scene scene, LoadSceneMode mode) {
+
+        animator.SetTrigger("FadeInAnim");
+
+    }
+
+    public void SceneFadeOut() {
+
+        animator.SetTrigger("FadeOutAnim");
+
+    }
+
+    private void Awake()
+    {
+        //SceneManager.sceneLoaded += SceneFadeIn;
+    }
 
 
 }
